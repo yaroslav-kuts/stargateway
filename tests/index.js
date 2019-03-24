1af72aaac5c3e4c66357da05e9314ba8cb2d9964
zzzonked <yaroslavkuts@gmail.com>
2019-03-24 18:28:45 +0200

    actualize tests

--- tests/index.js
+++ tests/index.js
@@ -23,17 +23,17 @@ describe('/spaceships', () => {
     describe('should return status 200 OK', () => {
       it('for not empty spaceships list', async () => {
           await factory.createMany('spaceship', 5);
-          const { status, body: { data } } = await agent.get('/spaceships');
+          const { status, body: { spaceships } } = await agent.get('/spaceships');
           status.should.equal(200);
-          data.length.should.equal(5);
+          spaceships.length.should.equal(5);
       });
     });
 
     describe('should return status 200 OK', () => {
       it('for empty spaceships list', async () => {
-          const { status, body: { data } } = await agent.get('/spaceships');
+          const { status, body: { spaceships } } = await agent.get('/spaceships');
           status.should.equal(200);
-          data.length.should.equal(0);
+          spaceships.length.should.equal(0);
       });
     });
   });
@@ -43,7 +43,7 @@ describe('/spaceships', () => {
       it('for existing spaceship', async () => {
           const spaceship = await factory.create('spaceship');
           const { 
-            status, body: { data: { name, sector } }
+            status, body: { name, sector }
           } = await agent.get(`/spaceships/${spaceship._id}`);
           status.should.equal(200);
           name.should.equal(spaceship.name);
@@ -56,7 +56,6 @@ describe('/spaceships', () => {
           const spaceship = await factory.build('spaceship');
           const { status, body: { message } } = await agent.get(`/spaceships/${spaceship._id}`);
           status.should.equal(404);
-          message.should.equal('There is no such spaceship');
       });
     });
   });
